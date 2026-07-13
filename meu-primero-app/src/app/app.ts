1cd1526f20c988ec5c287a0f08ce0ec68db0f809
JeBrandao <jebrandao@firjan.com.br>
2026-07-13 14:25:58 -0300

    Aula 06

--- meu-primero-app/src/app/app.ts
+++ meu-primero-app/src/app/app.ts
@@ -1,12 +1,15 @@
- import { Component, signal } from '@angular/core';
-  import { RouterOutlet, RouterLink } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { usuarioLogado, login, logout } from './core/auth';
 
-  @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet, RouterLink],
-    templateUrl: './app.html',
-    styleUrl: './app.css',
-  })
-  export class App {
-    protected readonly title = signal('meu-primeiro-app');
-  }
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, RouterLink],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+  usuarioLogado = usuarioLogado;
+  login = login;
+  logout = logout;
+}
